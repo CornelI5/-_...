eaf4031394488e8eacb6aa25e1f1953757865f64
CornelI5 <kenzoyooo0@gmail.com>
2026-07-19 18:16:21 +0700

    Update textual_decay.c

--- farland/textual_decay.c
+++ farland/textual_decay.c
@@ -1,12 +1,16 @@
-#include "textual_decay.h"
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void farland_decay(char* str) {
-    size_t len = strlen(str);
-    for (size_t i = 0; i < len; i++) {
-        if ((rand() % 100) < 5) { // 5% chance of decay
-            str[i] = '~'; 
+void apply_textual_decay(char* str, int decay_level) {
+    if (decay_level <= 0) return;
+    
+    int len = strlen(str);
+    char glitch_glyphs[] = "@#$_-^&*~+=?";
+    
+    for (int i = 0; i < len; i++) {
+        if ((rand() % 100) < decay_level) {
+            str[i] = glitch_glyphs[rand() % strlen(glitch_glyphs)];
         }
     }
 }
